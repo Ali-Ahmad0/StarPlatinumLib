095c85f6128106a33d1041608a722ccb1b5fce60
MuhammadWaleed <waleedraja2008@gmail.com>
2024-11-27 12:04:54 +0500

    InputMap for keyboard added.

--- res/test/scenes/TestScene.cpp
+++ res/test/scenes/TestScene.cpp
@@ -1,5 +1,14 @@
 #include "TestScene.hpp"
 #include "../../../src/main/Game.hpp"
+#include "../../../src/input/InputMap.hpp"
+
+void SetupKeyBindings() {
+    
+    InputMap::BindKey("MoveLeft", SDLK_LEFT);
+    InputMap::BindKey("MoveRight", SDLK_RIGHT);
+    InputMap::BindKey("MoveUp", SDLK_UP);
+    InputMap::BindKey("MoveDown", SDLK_DOWN);
+}
 
 void TestScene::Ready()
 {
@@ -10,6 +19,7 @@ void TestScene::Ready()
 	tilemap.LoadMap("res/test/assets/level/untitled.json");
 
 	tilemap.AddCollision(1, { 28, 31, 37, 81, 82, 84, 89, 90 });
+    SetupKeyBindings();
 }
 
 void TestScene::Update(double delta)
@@ -19,7 +29,61 @@ void TestScene::Update(double delta)
 
 void TestScene::Events(SDL_Event event)
 {
+    
 	player.Events(event);
+    // Test key press
+    if (InputMap::IsKeyJustPressed("MoveUp", event)) {
+        std::cout << "Up key just pressed" << std::endl;
+    }
+    if (InputMap::IsKeyJustPressed("MoveLeft", event)) {
+        std::cout << "MoveLeft key just pressed" << std::endl;
+    }
+    if (InputMap::IsKeyJustPressed("MoveRight", event)) {
+        std::cout << "MoveRight key just pressed" << std::endl;
+    }
+    if (InputMap::IsKeyJustPressed("MoveDown", event)) {
+        std::cout << "MoveDown key just pressed" << std::endl;
+    }
+
+    // Test key release
+    if (InputMap::IsKeyJustReleased("Jump", event)) {
+        std::cout << "Jump key released" << std::endl;
+    }
+    if (InputMap::IsKeyJustReleased("MoveLeft", event)) {
+        std::cout << "MoveLeft key released" << std::endl;
+    }
+    if (InputMap::IsKeyJustReleased("MoveRight", event)) {
+        std::cout << "MoveRight key released" << std::endl;
+    }
+
+    // Test key in released state
+    if (InputMap::IsKeyReleased("MoveUp")) {
+        std::cout << "Up key in released state" << std::endl;
+    }
+    if (InputMap::IsKeyReleased("MoveLeft")) {
+        std::cout << "MoveLeft key in released state" << std::endl;
+    }
+    if (InputMap::IsKeyReleased("MoveRight")) {
+        std::cout << "MoveRight key in released state" << std::endl;
+    }
+    if (InputMap::IsKeyReleased("MoveDown")) {
+        std::cout << "MoveDown key in released state" << std::endl;
+    }
+
+    // Test key press
+    if (InputMap::IsKeyPressed("MoveUp")) {
+        std::cout << "Up key pressed" << std::endl;
+    }
+    if (InputMap::IsKeyPressed("MoveLeft")) {
+        std::cout << "MoveLeft key pressed" << std::endl;
+    }
+    if (InputMap::IsKeyPressed("MoveRight")) {
+        std::cout << "MoveRight key pressed" << std::endl;
+    }
+    if (InputMap::IsKeyPressed("MoveDown")) {
+        std::cout << "MoveDown key pressed" << std::endl;
+    }
+
 }
 
 void TestScene::Draw()
--- src/input/InputMap.hpp
+++ src/input/InputMap.hpp
@@ -6,10 +6,57 @@
 
 class InputMap {
 public:
-    static void BindKey(const std::string& key, const SDL_Keycode& keycode);
+    static void BindKey(const std::string& key, const SDL_Keycode& keycode) {
+        bindings[key] = keycode;
+    }
 
-    static bool IsKeyPressed(const std::string& key);
-    static bool IsKeyReleased(const std::string& key);
+    static bool IsKeyJustPressed(const std::string& key, SDL_Event event) {
+        auto it = bindings.find(key);
+        if (it != bindings.end()) {
+            if (event.type == SDL_KEYDOWN && event.key.keysym.sym == it->second) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    static bool IsKeyJustReleased(const std::string& key, SDL_Event event) {
+        auto it = bindings.find(key);
+        if (it != bindings.end()) {
+            if (event.type == SDL_KEYUP && event.key.keysym.sym == it->second) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static bool IsKeyReleased(const std::string& key) {
+        const Uint8* state = SDL_GetKeyboardState(NULL);
+        auto it = bindings.find(key);
+        if (it != bindings.end()) {
+            SDL_Scancode scancode = SDL_GetScancodeFromKey(it->second);
+            if (state[scancode] == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static bool IsKeyPressed(const std::string& key) {
+        const Uint8* state = SDL_GetKeyboardState(NULL);
+        auto it = bindings.find(key);
+        if (it != bindings.end()) {
+            SDL_Scancode scancode = SDL_GetScancodeFromKey(it->second);
+            if (state[scancode] == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
 
 private:
     static std::unordered_map<std::string, SDL_Keycode> bindings;

187db2ffdbc1756a5a4d8a52d54218a7b13a5b53
MuhammadWaleed <waleedraja2008@gmail.com>
2024-11-30 23:43:54 +0500

    Added changes to the inputmap, reimplementation of methods and new additions too.

--- src/input/InputMap.hpp
+++ src/input/InputMap.hpp
@@ -1,63 +1,20 @@
+// InputMap.hpp
 #pragma once
 #include <SDL.h>
 #include <iostream>
 #include <unordered_map>
 #include <functional>
+#include "../common/Utils.hpp"
 
-class InputMap {
+class InputMap 
+{
 public:
-    static void BindKey(const std::string& key, const SDL_Keycode& keycode) {
-        bindings[key] = keycode;
-    }
-
-    static bool IsKeyJustPressed(const std::string& key, SDL_Event event) {
-        auto it = bindings.find(key);
-        if (it != bindings.end()) {
-            if (event.type == SDL_KEYDOWN && event.key.keysym.sym == it->second) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    static bool IsKeyJustReleased(const std::string& key, SDL_Event event) {
-        auto it = bindings.find(key);
-        if (it != bindings.end()) {
-            if (event.type == SDL_KEYUP && event.key.keysym.sym == it->second) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static bool IsKeyReleased(const std::string& key) {
-        const Uint8* state = SDL_GetKeyboardState(NULL);
-        auto it = bindings.find(key);
-        if (it != bindings.end()) {
-            SDL_Scancode scancode = SDL_GetScancodeFromKey(it->second);
-            if (state[scancode] == 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static bool IsKeyPressed(const std::string& key) {
-        const Uint8* state = SDL_GetKeyboardState(NULL);
-        auto it = bindings.find(key);
-        if (it != bindings.end()) {
-            SDL_Scancode scancode = SDL_GetScancodeFromKey(it->second);
-            if (state[scancode] == 1) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-
+    static void BindKey(const std::string& key, SDL_Scancode scancode);
+    static bool IsKeyPressed(const std::string& key);
+    static bool IsKeyReleased(const std::string& key);
+    static int GetDirection(const std::string& negativeKey, const std::string& positiveKey);
+    static Vector2& GetVector(const std::string& leftKey, const std::string& rightKey, const std::string& upKey, const std::string& downKey);
 
 private:
-    static std::unordered_map<std::string, SDL_Keycode> bindings;
+    static std::unordered_map<std::string, SDL_Scancode> bindings;
 };

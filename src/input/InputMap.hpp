48b38c59c415a913d75d4d441d9ca1488e1dee45
MuhammadWaleed <waleedraja2008@gmail.com>
2024-12-02 18:59:58 +0500

    minor changes

--- src/input/InputMap.hpp
+++ src/input/InputMap.hpp
@@ -12,7 +12,8 @@ public:
 
     static bool IsKeyReleased(const std::string& key);
     static bool IsKeyPressed(const std::string& key);
-
+    static int GetDirection(const std::string& negativeKey, const std::string& positiveKey);
+    static Vector2& GetVector(const std::string& leftKey, const std::string& rightKey, const std::string& upKey, const std::string& downKey);
 private:
     static std::unordered_map<std::string, SDL_Scancode> bindings;
 };

d4c7ee38ba2ab5c73be6c0e4222e672695f772c6
MuhammadWaleed <waleedraja2008@gmail.com>
2024-12-02 14:15:52 +0500

    fix merge

--- src/input/InputMap.hpp
+++ src/input/InputMap.hpp
@@ -1,62 +1,17 @@
-// InputMap.hpp
 #pragma once
+
 #include <iostream>
 #include <SDL.h>
 #include <unordered_map>
-#include <functional>
 #include "../common/Utils.hpp"
 
 class InputMap 
 {
 public:
-    static void BindKey(const std::string& key, const SDL_Keycode& keycode) {
-        bindings[key] = keycode;
-    }
+    static void BindKey(const std::string& key, SDL_Scancode keycode);
 
-    static bool IsKeyJustPressed(const std::string& key, SDL_Event event) {
-        auto it = bindings.find(key);
-        if (it != bindings.end()) {
-            if (event.type == SDL_KEYDOWN && event.key.keysym.sym == it->second) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    static bool IsKeyJustReleased(const std::string& key, SDL_Event event) {
-        auto it = bindings.find(key);
-        if (it != bindings.end()) {
-            if (event.type == SDL_KEYUP && event.key.keysym.sym == it->second) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static bool IsKeyReleased(const std::string& key) {
-        const Uint8* state = SDL_GetKeyboardState(NULL);
-        auto it = bindings.find(key);
-        if (it != bindings.end()) {
-            SDL_Scancode scancode = SDL_GetScancodeFromKey(it->second);
-            if (state[scancode] == 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static bool IsKeyPressed(const std::string& key) {
-        const Uint8* state = SDL_GetKeyboardState(NULL);
-        auto it = bindings.find(key);
-        if (it != bindings.end()) {
-            SDL_Scancode scancode = SDL_GetScancodeFromKey(it->second);
-            if (state[scancode] == 1) {
-                return true;
-            }
-        }
-        return false;
-    }
+    static bool IsKeyReleased(const std::string& key);
+    static bool IsKeyPressed(const std::string& key);
 
 
 
